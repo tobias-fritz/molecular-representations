dbf15d815f26e76fc45db53b304e9f85677db3d8
Tobias Fritz <fritz.tobias@mail.de>
2025-02-11 12:34:18 +0100

    Add matplotlib as a dependency and update package version in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="molecular_representations",
+    version="0.1.0",
     packages=find_packages(where="src"),
-    package_dir={"": "src"}
+    package_dir={"": "src"},
+    install_requires=[
+        "matplotlib>=3.7.1",
+    ],
 )
